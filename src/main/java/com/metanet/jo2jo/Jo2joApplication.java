275a5526efb18e6c6eb532938f2c3b60f0ac90ae
sungmin69355 <sungmin69355@naver.com>
2022-01-18 22:50:35 +0900

    first commit

---
+++ src/main/java/com/metanet/jo2jo/Jo2joApplication.java
@@ -0,0 +1,13 @@
+package com.metanet.jo2jo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jo2joApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jo2joApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metanet/jo2jo/Jo2joApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metanet.jo2jo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jo2joApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
