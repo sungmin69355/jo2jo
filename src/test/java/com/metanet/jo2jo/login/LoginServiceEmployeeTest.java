923b6ef32fbac8ef2456e076fea08cf857b7cc95
jmk <whalsrnraos@naver.com>
2022-01-28 13:17:27 +0900

    testcode "컴밋"

--- src/test/java/com/metanet/jo2jo/login/LoginServiceEmployeeTest.java
+++ src/test/java/com/metanet/jo2jo/login/LoginServiceEmployeeTest.java
@@ -23,7 +23,7 @@ public class LoginServiceEmployeeTest {
     public void adminLoginTest(){
         //given
         LoginDto loginDto = new LoginDto();
-        loginDto.setId("test");
+        loginDto.setId("1003");
         loginDto.setPassword("1234");
 
         //when
