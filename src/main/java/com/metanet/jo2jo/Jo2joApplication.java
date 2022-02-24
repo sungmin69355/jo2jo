2f3ac69c713e7c03f7ae22cdf87cbd210a6f2234
sungmin69355 <sungmin69355@naver.com>
2022-02-24 09:25:01 +0900

    fix: 배포환경 구축

--- src/main/java/com/metanet/jo2jo/Jo2joApplication.java
+++ src/main/java/com/metanet/jo2jo/Jo2joApplication.java
@@ -2,9 +2,15 @@ package com.metanet.jo2jo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Jo2joApplication {
+public class Jo2joApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Jo2joApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Jo2joApplication.class, args);
